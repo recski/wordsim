3c6d130d09ed9d2ca7e196fd5ec1523c13702a39
Gabor Recski <recski@mokk.bme.hu>
2016-02-24 16:42:21 +0100

    now includes machinesim

--- src/wordsim/models.py
+++ src/wordsim/models.py
@@ -27,8 +27,40 @@ class DummyModel(Model):
     def featurize(self, w1, w2):
         return dict([(k, random.random()) for k in ('a', 'b', 'c', 'd')])
 
+class MachineSimilarity():
+
+    def __init__(self, sim_name, section, cfg):
+        from fourlang.lexicon import Lexicon  # nopep8
+        from fourlang.similarity import WordSimilarity as FourlangWordSimilarity  # nopep8
+        self.fourlang_sim = FourlangWordSimilarity(cfg, section)
+        self.sim_name = sim_name
+        self.sim_types = cfg.get(section, 'sim_types').split('|')
+        for sim_type in self.sim_types:
+            if sim_type not in FourlangWordSimilarity.sim_types:
+                raise Exception(
+                    'unknown 4lang similarity: {0}'.format(sim_type))
+
+    def get_word_sims(self):
+        word_sims = {}
+        for sim_type in self.sim_types:
+            sim_name = "{0}_{1}".format(sim_type, self.sim_name)
+            word_sims[sim_name] = self.fourlang_sim.sim_type_to_function(
+                sim_type)
+        return word_sims
+
+class MachineModel(Model):
+    def __init__(self, conf, name):
+        self.ms = MachineSimilarity(name, name, conf)
+        self.sim_functions = self.ms.get_word_sims()
+
+    def featurize(self, w1, w2):
+        return dict(
+            [(name, fnc(w1, w2))
+             for name, fnc in self.sim_functions.iteritems()])
+
 def get_models(conf):
     models = []
+    models.append(MachineModel(conf, 'similarity_machine_longman'))
     for e_type in conf.options('embeddings'):
         fn = conf.get('embeddings', e_type)
         path = os.path.join(
